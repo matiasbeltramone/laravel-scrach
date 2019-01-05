0215dc08594f207916d44918dda277f68661aa22
Matias Beltramone <maatisk@gmail.com>
2019-01-04 21:21:47 -0300

    Routing Conventions Worth Following

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,28 @@ Route::get('/', 'PagesController@home');
 Route::get('/about', 'PagesController@about');
 Route::get('/contact', 'PagesController@contact');
 
-Route::get('/projects', 'ProjectsController@index');
-Route::post('/projects', 'ProjectsController@store');
-Route::get('/projects/create', 'ProjectsController@create');
+/*
+ * Convenciones en Laravel.
+ * GET /projects (index) Representa dame todos los datos de proyectos
+ * GET /projects/create (create) Para ver un formulario que nos haga cargar los datos del proyecto
+ * GET /projects/{id} (show) Ver X proyecto ya sea por un HTML o un JSON. Es decir ver uno en particular.
+ * POST /projects (store) Representa un endpoint para guardar un resource como en este caso es Proyecto.
+ * GET /projects/{id}/edit (edit) Si queremos editar un proyecto determinado la vista.
+ * PATCH /projects/{id} (update) Ya que representa que quiero actualizar un proyecto con el id 1 por ej.
+ * DELETE /projects/{id} (destroy) Significa que quiero eliminar un proyecto con X id.
+ */
+
+//Route::get('/projects', 'ProjectsController@index'); // Represents a resource
+//Route::get('/projects/create', 'ProjectsController@create');
+//Route::get('/projects/{id}', 'ProjectsController@show'); // Represents a resource
+//Route::post('/projects', 'ProjectsController@store');
+//Route::get('/projects/{id}/edit', 'ProjectsController@edit');
+//Route::patch('/projects/{id}', 'ProjectsController@update');
+//Route::delete('/projects/{id}', 'ProjectsController@destroy');
+
+// OR ANOTHER OPTION IT'S
+
+Route::resource('projects', 'ProjectsController');
 
 //Route::get('/', function () { //This is a closure function
 //    $tasks = [
